1348494eaf90df77103ec9047776be9592923963
Sunkwan Kim <47778502+danielkim428@users.noreply.github.com>
2024-05-19 11:19:51 -0400

    initialized spring boot

---
+++ Server/husksheets/src/main/java/com/valid/husksheets/HusksheetsApplication.java
@@ -0,0 +1,13 @@
+package com.valid.husksheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HusksheetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HusksheetsApplication.class, args);
+	}
+
+}
---
+++ Server/husksheets/src/test/java/com/valid/husksheets/HusksheetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valid.husksheets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HusksheetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
