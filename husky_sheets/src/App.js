5c33a085285af3f1d07d9248b6712d3d4b590e9a
ScarlettBlaize <cutiepie.ats@gmail.com>
2024-05-22 01:46:33 -0400

    cleaned up repo

--- husky_sheets/src/App.js
+++ husky_sheets/src/App.js
@@ -1,36 +1,6 @@
-// import React from 'react';
-// //import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
-// import { BrowserRouter as Link} from 'react-router-dom';
-// import './App.css';
-// //import HomePage from './HomePage';
-
-// function App() {
-//     return (
-//         //<Router>
-//             <div className="App">
-//                 <header className="App-header">
-//                     <h1>Welcome to the login page</h1>
-//                 </header>
-//                 <div className="App-content">
-//                     <Link to="/home">
-//                         <button>Press here for HuskSheet access</button>
-//                     </Link>
-//                 </div>
-//             </div>
-//             /* <Routes>
-//                 <Route path="/home" element={<HomePage />} />
-//             </Routes> */
-//         //</Router>
-//     );
-// }
-
-//  export default App;
-
 import React from "react";
-//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import MyButton from "./MyButton";
-//import HomePage from "./HomePage";
 
 function App() {
   return (
@@ -47,7 +17,6 @@ function App() {
         </a>
       </header>
       <header className="App-content">
-        {/* <MyButton to="/" text="Home" /> */}
         <MyButton to="home_page" text="Access the sheet" />
       </header>
     </div>
--- husky_sheets/src/MyButton.js
+++ husky_sheets/src/MyButton.js
@@ -1,31 +1,3 @@
-// const MyButton = ({ to }) => { 
-  
-//     return ( 
-//         <a href={`/${to}`}> 
-//             <button className="my-button"> 
-//                 Take me to {to === '' ? "home" : to} 
-//             </button> 
-//         </a> 
-//     ) 
-// } 
-  
-// export default MyButton;
-
-// import React from 'react';
-// import { useNavigate } from 'react-router-dom';
-
-// function MyButton({ to, children }) {
-//   let navigate = useNavigate();
-
-//   return (
-//     <button onClick={() => navigate(to)}>
-//       {children}
-//     </button>
-//   );
-// }
-
-// export default MyButton;
-
 import React from "react";
 import { Link } from "react-router-dom";
 
