5abe6c93a6a7d784f496e93824250b58cdfae921
ihysi2024 <138328084+ihysi2024@users.noreply.github.com>
2024-06-08 13:50:37 -0400

    operation logic

--- husky_sheets/src/ParsingUtils/AllOperationLogic.tsx
+++ husky_sheets/src/ParsingUtils/AllOperationLogic.tsx
@@ -130,6 +130,97 @@ export default class FormulaParseTS {
     }
   }
 
-  // Additional operations and methods can be added here
+  public sum(stringList: string[]): number {
+    const doubleList: number[] = [];
+    let output: number = 0;
+    for (let i = 0; i < stringList.length; i++) {
+      try {
+        const num: number = parseFloat(stringList[i]);
+        doubleList.push(num);
+      } catch (error) {
+        throw new Error("Cannot sum non-numbers");
+      }
+    }
+    for (let j = 0; j < doubleList.length; j++) {
+      output += doubleList[j];
+    }
+    return output;
+  }
+
+  public min(stringList: string[]): number {
+    const doubleList: number[] = [];
+    let minimum: number = Number.POSITIVE_INFINITY;
+    for (let i = 0; i < stringList.length; i++) {
+      try {
+        const num: number = parseFloat(stringList[i]);
+        doubleList.push(num);
+      } catch (error) {
+        throw new Error("Cannot find the minimum of non-numbers");
+      }
+    }
+    for (let j = 0; j < doubleList.length; j++) {
+      if (doubleList[j] < minimum) {
+        minimum = doubleList[j];
+      }
+    }
+    return minimum;
+  }
+
+  ////
+  public max(stringList: string[]): number {
+    const doubleList: number[] = [];
+    let maximum: number = Number.NEGATIVE_INFINITY;
+    for (let i = 0; i < stringList.length; i++) {
+      try {
+        const num: number = parseFloat(stringList[i]);
+        doubleList.push(num);
+      } catch (error) {
+        throw new Error("Cannot find the maximum of non-numbers");
+      }
+    }
+    for (let j = 0; j < doubleList.length; j++) {
+      if (doubleList[j] > maximum) {
+        maximum = doubleList[j];
+      }
+    }
+    return maximum;
+  }
+
+  public avg(stringList: string[]): number {
+    const doubleList: number[] = [];
+    let average: number = 0;
+    for (let i = 0; i < stringList.length; i++) {
+      try {
+        const num: number = parseFloat(stringList[i]);
+        doubleList.push(num);
+      } catch (error) {
+        throw new Error("Cannot find the average of non-numbers");
+      }
+    }
+
+    if (doubleList.length === 0) {
+      return 0;
+    }
+
+    for (let j = 0; j < doubleList.length; j++) {
+      average += doubleList[j];
+    }
+    average = average / doubleList.length;
+    return average;
+  }
+
+
+  public concat(stringList: string[]) {
+    var final = ""
+    for (let i = 0; i < stringList.length; i++) {
+      final = final.concat(stringList[i]);
+    }
+    return final;
+  }
+
+
+  public debug(String e) {
+    return e;
+  }
 
 }
