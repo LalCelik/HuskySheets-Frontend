82b1456054b5e258f2be84e0a2c1c4d82a9862a2
ihysi2024 <138328084+ihysi2024@users.noreply.github.com>
2024-06-13 10:23:41 -0400

    get updates

--- husky_sheets/src/Sheet.tsx
+++ husky_sheets/src/Sheet.tsx
@@ -20,6 +20,7 @@ import {useParams} from "react-router-dom";
  * @returns a grid of cells
  */
 function Sheet() {
+  console.log("BEGINNING AGAIN");
   const [message, setMessage] = useState("");
   const gridContainerRef = useRef(null);
   const previousValues = useRef({});
@@ -35,7 +36,12 @@ function Sheet() {
   const cellNamePattern = /^[A-Z]+[0-9]+$/;
   const {sheetName} = useParams();
   const {dataPublisher} = useParams();
+  const [publishedData, setPublishedData] = useState(false);
 
+  const [url, setUrl] = useState('http://localhost:8080/api/v1/getUpdatesForSubscription'); // Define state variable for URL
+  const [updateUrl, setUpdateUrl] = useState('http://localhost:8080/api/v1/updateSubscription'); // Define state variable for URL
+
+  const [combineData, setCombinedData] = useState(false);
 
   const user = document.cookie;
   if (user === "") {
@@ -45,6 +51,23 @@ function Sheet() {
   const password = user.split(":")[1];
   const base64encodedData = Buffer.from(`${username}:${password}`).toString('base64');
 
+  console.log(dataPublisher + user);
+
+
+
+
+  if (dataPublisher != username) {
+    //console.log(sheetUrlEnding);
+    //sheetUrlEnding = "getUpdatesForPublished";
+    //updateSheetUrlEnding = "updatePublished";
+  }
+  else {
+    //sheetUrlEnding = "getUpdatesForSubscription";
+    //updateSheetUrlEnding = "updateSubscription";
+  }
+
+  //console.log(sheetUrlEnding);
+  //console.log(updateSheetUrlEnding);
   /**
   function getCellsInFormula(expression) {
     const pattern =  "(IF|SUM|MIN|MAX|AVG|CONCAT|DEBUG|:|<|>|<>|&|=|\\+|,|\\*|-|\\/|\\||\\(|\\)|[A-Z]+\\d+)";
@@ -106,7 +129,7 @@ function Sheet() {
         cell.removeEventListener('focus', handleCellFocus);
         cell.removeEventListener('blur', handleCellBlur);
         cell.removeEventListener('input', handleCellInput);
-
+        
         cell.addEventListener('focus', handleCellFocus);
         cell.addEventListener('blur', handleCellBlur);
         cell.addEventListener('input', handleCellInput);
@@ -131,12 +154,18 @@ function Sheet() {
     const columnName = generateColumnName(cellIndex);
     const rowIndex = cell.parentElement.rowIndex - 1;
 
+    console.log(rowIndex, cellIndex);
     const gridInstance = gridContainerRef.current;
+    console.log("IN THE CELL FOCUS - gridinstance", gridInstance);
     var tds = gridInstance.querySelectorAll("td[data-column-id='" + columnName.toLowerCase() + "']");
+    console.log("IN THE CELL FOCUS - tds", tds);
     tds[rowIndex].innerHTML = data[rowIndex][cellIndex];
+    console.log("IN THE CELL FOCUS - tds at row index", tds[rowIndex].innerHTML);
+    console.log("IN THE CELL FOCUS - data at r/c", data[rowIndex][cellIndex]);
   };
 
   const handleCellBlur = (event) => {
+    console.log("GET TO BLUR");
     const cell = event.target;
     // const newValue = cell.textContent.toUpperCase();
     const newValue = cell.textContent;
@@ -205,23 +234,7 @@ function Sheet() {
     // console.log('Cell value is changing:', event.target.textContent);
   };
  
-  /**
-  const generateColumnName = (index) => {
-    let columnName = '';
-    while (index > 0) {
-      let remainder = index % 26;
-      if (remainder === 0) {
-        columnName = 'Z' + columnName;
-        index = Math.floor(index / 26) - 1;
-      } else {
-        columnName = String.fromCharCode(64 + remainder) + columnName;
-        index = Math.floor(index / 26);
-      }
-    }
-    return columnName;
-  };
 
-  **/
 
   const columns = [
     {
@@ -312,9 +325,14 @@ function Sheet() {
     */
 
   function reverseParse(listUpdates) {
+    var finalData = Array.from({ length: 100 }, (_, rowIndex) => {
+      const row = Array.from({ length: 100 - 1 }, () => '')
+      return [rowIndex, ...row];
+    });
     var cellsWithFormulas = []
     const regex = /([A-Z]+)(\d+)/;
-
+    //console.log("PRE", displayData);
+  
     for (let idx=0; idx < listUpdates.length; idx++) {
       const splitStr = listUpdates[idx].split(" ");
       const match = regex.exec(splitStr[0]);
@@ -340,19 +358,53 @@ function Sheet() {
       const colIdx = columnNameToIndex(cellCoords.letter);
       const rowIdx = cellCoords.number;
       splitStr[0] = "";
+      if ( splitStr.join("").includes("COPY")) {
+
+      }
+      else {
+        displayData[rowIdx][colIdx] = '' + FormulaParse(RefToNumberFormula(cellNamePattern, dels, data, splitStr.join(""))).value;
+      }
       // console.log(RefToNumberFormula(cellNamePattern, dels, data, splitStr.join("")));
-      displayData[rowIdx][colIdx] = '' + FormulaParse(RefToNumberFormula(cellNamePattern, dels, data, splitStr.join(""))).value;
     }
-    return displayData;
+
+    for (let rowIdx = 0; rowIdx < displayData.length; rowIdx++) {
+      for (let colIdx = 0; colIdx < displayData[rowIdx].length; colIdx++) {
+        finalData[rowIdx][colIdx] = displayData[rowIdx][colIdx];
+      }
+    }
+    
+    const pub = getPublished();
+    //console.log("HELLO" , getPublished())
+
+
+    console.log("HERE", pub)
+    
+    //getPublished().map(item => {console.log(item['1'])
+    //})
+    /**
+    for (let rIdx = 0; rIdx < 1; rIdx++) {
+      console.log("********", getPublished()[0])
+      for (let cIdx = 0; cIdx < 10; cIdx) {
+        console.log(getPublished()[rIdx][cIdx]);
+        finalData[rIdx][cIdx] = getPublished()[rIdx][cIdx]
+      }
+    }
+
+    **/
+   
+  
+    //finalData = getPublished();
+
+    return finalData;
   }
 
-  const saveSheetUpdates = () => {
-    var stringEmpListUpdates = "";
-    for (let i = 0; i < empListUpdates.length; i++) {
-      stringEmpListUpdates = stringEmpListUpdates + empListUpdates[i];
-    }
-    console.log(stringEmpListUpdates);
-    fetch("http://localhost:8080/api/v1/updatePublished", {
+  const getPublished = () => {
+    const regex = /([A-Z]+)(\d+)/;
+    var d = Array.from({ length: 100 }, (_, rowIndex) => {
+      const row = Array.from({ length: 100 - 1 }, () => '')
+      return [rowIndex, ...row];
+    });
+    fetch('http://localhost:8080/api/v1/getUpdatesForPublished', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
@@ -362,7 +414,78 @@ function Sheet() {
       body: JSON.stringify({
         // publisher: "user3",
         // sheet: "Example Sheet",
-        publisher: username,
+        publisher: dataPublisher,
+        sheet: sheetName,
+        id: -1,
+        payload: "example payload",
+      })
+    })
+    .then(response => {
+      if (!response.ok) {
+        console.log(response);
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // Parse response body as JSON
+    })
+    .then(data => {
+      //console.log(data.value);
+
+      for (let sheetIdx = 0; sheetIdx < data.value.length; sheetIdx++) {
+        const allUpdates = data.value[sheetIdx].payload.split("\n");
+        for (let updateIdx = 0; updateIdx < allUpdates.length-1; updateIdx++) {
+          //console.log("UPDATES ****", allUpdates[updateIdx]);
+          //console.log(allUpdates[updateIdx])
+          const splitStr = allUpdates[updateIdx].split(" ");
+          
+          const match = regex.exec(splitStr[0]);
+          const cellCoords = {letter: match[1], number: parseInt(match[2], 10)};
+          const colIdx = columnNameToIndex(cellCoords.letter);
+          const rowIdx = cellCoords.number;
+          const cellIndex = columnNameToIndex(colIdx);
+          //console.log(splitStr[1]);
+          d[rowIdx][colIdx] = splitStr[1];
+
+          //return displayData;
+          //console.log(d)
+        }
+      }
+      //console.log("MIDDLE", d);
+      //return finalData;
+      // Handle success
+    })
+    .catch(error => {
+      console.error('Error posting data:', error);
+      // Handle error
+    });
+
+    //console.log("POST", d);
+    return d;
+  }
+
+  const saveSheetUpdates = () => {
+    var stringEmpListUpdates = "";
+    for (let i = 0; i < empListUpdates.length; i++) {
+      stringEmpListUpdates = stringEmpListUpdates + empListUpdates[i];
+    }
+   
+    if (username == dataPublisher) {
+      console.log("HERE");
+      setUpdateUrl('http://localhost:8080/api/v1/updatePublished')
+    }
+  
+    console.log(stringEmpListUpdates);
+    //console.log("http://localhost:8080/api/v1/" + updateSheetUrlEnding);
+    fetch(updateUrl, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + base64encodedData
+      },
+      body: JSON.stringify({
+        // publisher: "user3",
+        // sheet: "Example Sheet",
+        publisher: dataPublisher,
         sheet: sheetName,
         id: 0,
         payload: stringEmpListUpdates,
@@ -385,107 +508,96 @@ function Sheet() {
     });
   }
 
-  const shareSheet = () => {
-    setShare(true);
+  const getUpdates = () => {
+    setPublishedData(true);
+    if (username == dataPublisher) {
+      setUrl('http://localhost:8080/api/v1/getUpdatesForPublished')
+    }
+    console.log("HERE");
   }
 
-  const handleCloseDialog = () => {
-    setShare(false);
+  /**
+  const mergePublished = () => {
+    //console.log(displayData);
+    for (let rowIndex = 0; rowIndex < data.length; rowIndex++) {
+      for (let cellIndex = 0; cellIndex < data[rowIndex].length; cellIndex++) {
+        const columnName = generateColumnName(cellIndex);
+        const gridInstance = gridContainerRef.current;
+        var tds = gridInstance.querySelectorAll("td[data-column-id='" + columnName.toLowerCase() + "']");
+        //tds[rowIndex].innerHTML = data[rowIndex][cellIndex];
+        console.log(displayData[rowIndex][cellIndex]);
+        displayData[rowIndex][cellIndex] = tds[rowIndex].textContent;
+
+      }
+    }
+    return displayData
   }
 
-  const addUser = () => {
-    getSheets();
-    console.log(searchResults);
-  }
 
-  const handleSearchChange = (e) => {
-    const { value } = e.target;
-    setSearchUser(value);
-    
-    console.log(value);
-    console.log(searchResults)
-  
-  };
-
-  const handleSelectResult = (result) => {
-    console.log('Selected:', result);
-    handleCloseDialog();
-  };
-
-  const getSheets = () => {
-    fetch("http://localhost:8080/api/v1/getPublishers", {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: "Basic " + base64encodedData,
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        for (let i = 0; i < data.value.length; i++) {
-          searchResults.push(data.value[i].publisher);
-        };
-        setSearchResults(searchResults);
+/**
+  const getUpdates = () => {
+    var stringEmpListUpdates = "";
+    console.log(displayData);
+    for (let rowIndex = 1; rowIndex < displayData.length; rowIndex++) {
+      //console.log("ROW IDX*************", rowIndex);
+      for (let cellIndex = 1; cellIndex < displayData[rowIndex-1].length; cellIndex++) {
+          if (displayData[cellIndex][rowIndex].toString().length != 0) {
+            var columnName = generateColumnName(cellIndex);
+            console.log("DISPLAY DATA", displayData[cellIndex][rowIndex]);
+            const updateToAdd = `\$${columnName}${rowIndex - 1} ${displayData[cellIndex][rowIndex]}\n`;
+            //console.log(updateToAdd);
+            stringEmpListUpdates = stringEmpListUpdates.concat(updateToAdd)
+            //console.log(stringEpListUpdates);
+          }
+          }
+          }
+    console.log("EMPLIST UPDATES", stringEmpListUpdates);
+    if (username == dataPublisher) {
+      console.log(displayData);
+      //setUrl('http://localhost:8080/api/v1/getUpdatesForPublished');
+      fetch('http://localhost:8080/api/v1/getUpdatesForPublished', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Basic ' + base64encodedData
+        },
+        body: JSON.stringify({
+          // publisher: "user3",
+          // sheet: "Example Sheet",
+          publisher: dataPublisher,
+          sheet: sheetName,
+          id: -1,
+          payload: stringEmpListUpdates,
+        })
       })
-    };
+      .then(response => {
+        if (!response.ok) {
+          console.log(response);
+          throw new Error('Network response was not ok');
+        }
+        return response.json(); // Parse response body as JSON
+      })
+      .then(data => {
+        console.log('Post request successful:', data);
+        // Handle success
+      })
+      .catch(error => {
+        console.error('Error posting data:', error);
+        // Handle error
+      });
+    }
+  }
+    **/
 
+  
+  
+  
   return (
     <div className="panel">
       {/* Add your panel components here */}
       <button onClick={saveSheetUpdates}>Save</button>
-      <button onClick={shareSheet}>Share</button>
-       {/* Dialog box */}
-       {share && (
-        <div style={{ 
-          position: 'fixed', 
-          top: '50%', 
-          left: '50%', 
-          transform: 'translate(-50%, -50%)', 
-          backgroundColor: '#fff', 
-          border: '1px solid #ccc', /* Border style */
-          borderRadius: '8px', 
-          padding: '20px', 
-          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-          textAlign: 'center'
-        }}>
-          <div className="dialog-content">
-            {/* Dialog content */}
-            <p>This is a dialog box!</p>
-            
-            {/* Search bar */}
-            <input
-              type="text"
-              value={searchUser}
-              onChange={handleSearchChange}
-              placeholder="Search..."
-              style={{ marginBottom: '10px', padding: '5px', width: '100%' }}
-            />
-            
-            {/* Search results */}
-            {searchResults.length > 0 ? (
-              <ul style={{ listStyle: 'none', padding: 0 }}>
-                {searchResults.map((result, index) => (
-                  <li key={index} style={{ marginBottom: '5px', cursor: 'pointer' }} onClick={() => handleSelectResult(result)}>
-                    {result}
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>No results found</p>
-            )}
-          </div>
-          
-          {/* Buttons */}
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-            {/* Button to perform action 1 (Close) */}
-            <button style={{ marginRight: '10px' }} onClick={handleCloseDialog}>Close</button>
-            
-            {/* Button to perform action 2 (Share) */}
-            <button onClick={addUser}>Share</button>
-          </div>
-        </div>
-      )}
+      <button onClick={getUpdates}>Get Updates</button>
     <div ref={gridContainerRef}>
       <Grid
         columns={columns}
@@ -513,10 +625,9 @@ function Sheet() {
         server={{
           method: 'POST',
           headers: {'Accept': 'application/json','Content-Type': 'application/json','Authorization': 'Basic ' + base64encodedData},
-          url: 'http://localhost:8080/api/v1/getUpdatesForSubscription',
+          url:'http://localhost:8080/api/v1/getUpdatesForSubscription' ,
 
-          // body: '{"publisher": "user3","sheet": "Example Sheet","id": -1,"payload": "examplePayload"}',
-
+          // body: '{"publisher": "user3","sheet": "Example Sheet","id": -1,"payload": "examplePayload"}';
           body: JSON.stringify({
             publisher: dataPublisher,
             sheet: sheetName,
@@ -524,6 +635,9 @@ function Sheet() {
             payload: "examplePayload"
           }),
           then: data => {
+
+            console.log("PRINTING DATA", data);
+            //displayAllPublishedChanges(data.value);
             let resultList = data.value[0].payload.split("\n")
             resultList.pop() // Removes empty string at the end
             return reverseParse(resultList);
@@ -531,6 +645,7 @@ function Sheet() {
         }}
       />
     </div>
-  </div>);}
+  </div>);
+}
  
 export default Sheet;
